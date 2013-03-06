d442b6ebd2b877a27de01b20b3dcda6b32c1308a
Edgar Filipe Ramires dos Santos <edfil221@gmail.com>
2013-03-06 20:34:04 +0000

    Update Proj1.cpp
    Changed to classes

--- Proj1.cpp
+++ Proj1.cpp
@@ -4,79 +4,113 @@
 #include <stdlib.h>
 using namespace std;
 
+/** Node that represents a domino **/
 class Node {
     private:
-        queue<int> paths;
-        bool visited;
+        /** All possible knockdowns from this domino **/
+        queue<int> _paths;
+        /** If this domino was already knocked down **/
+        bool _visited;
 
     public:
-        Node(){ visited = false;}
+        Node(){ _visited = false;}
+        /** Add a possible knockdown from this domino **/
         void addPath (int);
-        bool isVisited() { return visited; };
-        void visit() { visited = true;}
-        queue<int> getPaths (){ return paths; }
+        /** Return if this domino was already knocked down **/
+        bool isVisited() { return _visited; };
+        /** Knockdown this domino **/
+        void visit() { _visited = true;}
+        /** Get all possible knockdowns from this domino **/
+        queue<int> getPaths (){ return _paths; }
+        /**  **/
         int removePath();
 };
 
-int nextUnvisitedNode(Node* array, int size){
-    for(int i = 0; i < size; i++){
-        if(!array[i].isVisited())
+/** Add a Path to a node **/
+void Node::addPath(int value){
+    _paths.push(value);
+}
+
+/** Remove and Return Path to a node **/
+int Node::removePath(){
+    if(_paths.empty())
+        return -1;
+    int answer = _paths.front();
+    _paths.pop();
+    return answer;
+}
+
+
+class DominoRun {
+    private:
+        int _numDominos;
+        int _numConnections;
+        int _manualDrops;
+        int _currentNode;
+        int _numVisited;
+        Node* _dominoArray;
+
+    public:
+        DominoRun();
+        int nextUnvisitedNode();
+        int cicle();
+        int getManualDrops() { return _manualDrops; }
+
+};
+
+DominoRun::DominoRun(){
+    int from, to;
+    cin >> _numDominos >> _numConnections;
+    _manualDrops = 1;
+    _currentNode = _numVisited = 0;
+    _dominoArray = new Node[_numDominos];
+    for(int i = 0; i < _numConnections; i++){
+        cin >> from >> to;
+        _dominoArray[from-1].addPath(to-1);
+    }
+}
+
+int DominoRun::nextUnvisitedNode(){
+    for(int i = 0; i < _numDominos; i++){
+        if(!_dominoArray[i].isVisited())
             return i;
     }
     return -1;
 }
 
+int DominoRun::cicle(){
+    while(true){
+        cout << "Visiting Node: " << _currentNode+1;
+        _dominoArray[_currentNode].visit();
+        _numVisited++;
+        if(_numVisited >= _numDominos)
+            return _manualDrops;
+        _currentNode = _dominoArray[_currentNode].removePath();
+        if(_currentNode == -1){
+            _currentNode = nextUnvisitedNode();
+            _manualDrops++;
+        }
+        cout << " , and going for: " << _currentNode+1 << endl;
+    }
+}
+
 int main ()
 {
-    int testCase, numDominos, numConnections, i = 0;
-    int from, to, numVisited = 0, manualDrops = 0;
+    int numberCases;
+    int *answers;
 
     // Ler quantos testes há
-    cin >> testCase;
-    // Ler o numero de Dominos e de Conections
-    cin >> numDominos >> numConnections;
+    cin >> numberCases;
+    answers = new int[numberCases];
 
-    Node* array = new Node[numDominos];
-
-    // Populate paths
-    for(i = 0; i < numConnections; i++){
-        cin >> from >> to;
-        array[from-1].addPath(to-1);
+    for(int i = 0; i < numberCases; i++){
+        DominoRun *dominoCase = new DominoRun();
+        answers[i] = dominoCase->cicle();
     }
 
-    int currentNode = 0;
-    manualDrops++;
-    while(true){
-        cout << "Visiting: " << currentNode+1 << endl;
-        array[currentNode].visit();
-        numVisited++;
-        if(numVisited >= numDominos)
-            break;
-        currentNode = array[currentNode].removePath();
-        if(currentNode == -1){
-            currentNode = nextUnvisitedNode(array, numDominos);
-            manualDrops++;
-        }
-    }
-
-    cout << "END -> " << manualDrops << endl;
-
-//    queue<Node>::const_iterator nodeIterator;
-//    for (nodeIterator = dominos.begin(); nodeIterator != dominos.end(); ++nodeIterator) { cout << i++; }
+    cout << "END" << endl;
+    for(int i = 0; i < numberCases; i++)
+        cout << answers[i] << endl;
 
   return 0;
-}
-
-/** Add a Path to a node **/
-void Node::addPath(int value){
-    paths.push(value);
-}
-
-/** Remove and Return Path to a node **/
-int Node::removePath(){
-    if(paths.empty())
-        return -1;
-    int answer = paths.front();
-    paths.pop();
-    return answer;
 }
